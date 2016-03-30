dacc642f4d1676d51076c52c245aaad94f7a67c1
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-03-30 11:13:30 +0200

    Add edit post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -17,7 +17,22 @@ class PostsController < ApplicationController
     redirect_to posts_path
   end
 
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:caption, :image)
