e86bde2311ca81359a54f76f9ae5a1e75773c558
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-04-06 14:54:44 +0200

    Add user authentication and user name above posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
+  before_action :owned_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -10,11 +11,12 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+
     if @post.save
       flash[:success] = "Your post has been created!"
       redirect_to @post
@@ -48,6 +50,13 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def owned_post
+      unless current_user == @post.user
+        flash[:alert] = "That post doesn't belong to you!"
+        redirect_to root_path
+      end
+  end
 
   def find_post
     @post = Post.find(params[:id])
