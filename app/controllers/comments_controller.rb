1d633675a1f8871701df2945fa003353b2b61a63
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-04-11 14:18:34 +0200

    Add comment model and controller + create and destroy actions

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "Comment was successfully created."
+      redirect_to :back
+    else
+      failed_action("Something went wrong. Check the comment form.")
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+
+    if @comment.destroy
+      flash[:success] = "Comment was succefully deleted."
+      redirect_to root_path
+    else
+      failed_action("Something went wrong. Comment was not deleted.")
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def failed_action(alert_message)
+    flash.now[:alert] = alert_message
+    render root_path
+  end
+end
