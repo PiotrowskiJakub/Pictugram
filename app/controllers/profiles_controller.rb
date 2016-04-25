b7b48666ba7145e9856529c1b085b52bdf55e90d
Jakub Piotrowski <piotrowski.jakub1993@gmail.com>
2016-04-25 23:00:56 +0200

    Add edit profiles page and ability to edit user profiles with avatar and bio information

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,31 @@
 class ProfilesController < ApplicationController
+  before_action :find_user
 
   def show
-    @posts = User.find_by(user_name: params[:user_name]).posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(profile_params)
+      flash[:success] = "Your profile has been updated."
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash.now[:alert] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find_by(user_name: params[:user_name])
+  end
+
+  def profile_params
+    params.require(:user).permit(:avatar, :bio)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: "152x152#" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
